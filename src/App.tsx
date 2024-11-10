b5065ee0c44be10c12d6933d13817a323889dab9
Atay <atte877@gmail.com>
2024-11-10 16:30:07 +0100

    remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 // src/App.tsx
 import React from "react";
-//import AddSnusLocation from './components/AddSnusLocation';
 import MapView from "./components/MapView";
-import NavBar from "./components/NavBar";
 
 const App: React.FC = () => {
   return (
